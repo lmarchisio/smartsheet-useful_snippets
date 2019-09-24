2ee0d234ea49d73dd3ec4c20f99b8d2690826e32
lmarchisio <51975294+lmarchisio@users.noreply.github.com>
2019-09-24 09:25:55 -0700

    Update list_users.py

--- list_users.py
+++ list_users.py
@@ -12,7 +12,6 @@ smart.errors_as_exceptions(True)
 # log all calls
 logging.basicConfig(filename='rwsheet.log', level=logging.INFO)
 
-# note that the returned list of users from smartsheet is iterable, which is why this differs from get_workspace.py
 response = smart.Users.list_users(include_all=True)
 users = response.data
 
@@ -20,4 +19,5 @@ for user in users:
     pyuser = json.loads(str(user))
     print(str(pyuser['id']) + ": " + str(pyuser['name']))
 
-
+print('press Enter key to close')
+input()
